6445f00c13826daa7e38fdb61dcf3898ac2e2e35
77954309 <77954309@qq.com>
2020-10-21 21:18:54 +0800

    sqoop

--- spring-linkis-web/src/test/java/com/lm/web/url/EntranceRestfulTest.java
+++ spring-linkis-web/src/test/java/com/lm/web/url/EntranceRestfulTest.java
@@ -44,7 +44,7 @@ public class EntranceRestfulTest {
      * 执行任务 execute
      */
     @Test
-    public void TestExecuteSql(){
+    public void testExecuteSql(){
         testLogin();
 
         String url = "/api/rest_j/v1/entrance/execute";
@@ -54,6 +54,45 @@ public class EntranceRestfulTest {
         map.put("executeApplicationName","hive");//执行引擎，我用的hive
         map.put("executionCode","select count(1) from linkis_db.test1");
         map.put("runType","sql");//当用户执行如spark服务时，可以选择python、R、SQL等,不能为空
+        //因为我没有执行文件脚本，所以没有scriptPath参数
+        String executeSql = "http://192.168.200.116:8088/api/rest_j/v1/entrance/execute";
+        ResponseEntity<JSONObject> jsonResponseEntity = restClient.postForEntity(executeSql, map, JSONObject.class);
+        System.out.println("状态码："+jsonResponseEntity.getStatusCodeValue());
+        JSONObject body = jsonResponseEntity.getBody();
+        System.out.println("body :" + body.toString());
+    }
+
+    @Test
+    public void testExecuteShell(){
+        testLogin();
+
+        String url = "/api/rest_j/v1/entrance/execute";
+        JSONObject map = new JSONObject();
+        map.put("method",url);
+        map.put("params",new HashMap<>()); //用户指定的运行服务程序的参数,必填，里面的值可以为空
+        map.put("executeApplicationName","shell");//执行引擎，我用的hive
+        map.put("executionCode","echo 'test'");
+        map.put("runType","shell");//当用户执行如spark服务时，可以选择python、R、SQL等,不能为空
+        //因为我没有执行文件脚本，所以没有scriptPath参数
+        String executeSql = "http://192.168.200.116:8088/api/rest_j/v1/entrance/execute";
+        ResponseEntity<JSONObject> jsonResponseEntity = restClient.postForEntity(executeSql, map, JSONObject.class);
+        System.out.println("状态码："+jsonResponseEntity.getStatusCodeValue());
+        JSONObject body = jsonResponseEntity.getBody();
+        System.out.println("body :" + body.toString());
+    }
+
+
+    @Test
+    public void testExecuteSqoop(){
+        testLogin();
+
+        String url = "/api/rest_j/v1/entrance/execute";
+        JSONObject map = new JSONObject();
+        map.put("method",url);
+        map.put("params",new HashMap<>()); //用户指定的运行服务程序的参数,必填，里面的值可以为空
+        map.put("executeApplicationName","sqoop");//执行引擎，我用的hive
+        map.put("executionCode","sqoop");
+        map.put("runType","sqoop");//当用户执行如spark服务时，可以选择python、R、SQL等,不能为空
         //因为我没有执行文件脚本，所以没有scriptPath参数
         String executeSql = "http://192.168.200.116:8088/api/rest_j/v1/entrance/execute";
         ResponseEntity<JSONObject> jsonResponseEntity = restClient.postForEntity(executeSql, map, JSONObject.class);
